8a75a83dc81fb092366cd9a169ec3b1ded8f9ca3
Akhil Nanavati <akhilnanavati23@gmail.com>
2025-07-04 09:40:25 +0530

    lifi working

---
+++ src/utils/bridgeAndBuy.js
@@ -0,0 +1,54 @@
+import { parseUnits, createPublicClient, http } from 'viem';
+import { erc20Abi } from 'viem';
+import { writeContract, switchChain } from '@wagmi/core';
+import { fetchQuote } from '../lifi/fetchQuote';
+import { approveTokenIfNeeded } from '../lifi/approveTokenIfNeeded';
+import { executeSwap } from '../lifi/executeSwap';
+import { USDC_BASE, USDC_LINEA, CHAIN_IDS, COMMERCE_CONTRACTS } from '../constants';
+import { abi as commerceAbi } from '../abi/Commerce.json';
+
+export const bridgeAndBuy = async ({ product, userAddress }) => {
+  const productChainId =
+    product.currency.toLowerCase() === USDC_LINEA.toLowerCase() ? CHAIN_IDS.linea : CHAIN_IDS.base;
+
+  const userChainId = productChainId === CHAIN_IDS.linea ? CHAIN_IDS.base : CHAIN_IDS.linea;
+  const fromToken = productChainId === CHAIN_IDS.linea ? USDC_BASE : USDC_LINEA;
+  const toToken = product.currency;
+
+  const publicClient = createPublicClient({
+    chain: { id: productChainId, name: '', nativeCurrency: {}, rpcUrls: { default: { http: ['https://rpc'] } } },
+    transport: http(),
+  });
+
+  const amount = parseUnits(String(product.price), 6).toString();
+
+  const balance = await publicClient.readContract({
+    address: toToken,
+    abi: erc20Abi,
+    functionName: 'balanceOf',
+    args: [userAddress],
+  });
+
+  if (BigInt(balance) < BigInt(amount)) {
+    const route = await fetchQuote({
+      fromChainId: userChainId,
+      toChainId: productChainId,
+      fromTokenAddress: fromToken,
+      toTokenAddress: toToken,
+      amount,
+      address: userAddress,
+    });
+
+    await approveTokenIfNeeded(route.steps[0]);
+    await executeSwap(route, () => {});
+  }
+
+  await switchChain({ chainId: productChainId });
+
+  await writeContract({
+    address: COMMERCE_CONTRACTS[productChainId],
+    abi: commerceAbi,
+    functionName: 'purchaseProduct',
+    args: [product.id],
+  });
+};
